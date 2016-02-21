1217e9f9cc4359786b1c9550a9b7c6c7245e50b7
Rachel <rnchurch@ucdavis.edu>
2016-02-21 14:40:12 -0800

    Have a basic collapse/un-collapse javascript working on the calendar

--- ECS193/static/js/calendar.js
+++ ECS193/static/js/calendar.js
@@ -1,17 +1,20 @@
 
 
-function BuildRNRow(count, chairs, startTime, lunchTime, duration, endTime) {
+function BuildRNRow(RNIndex, chairs, startTime, lunchTime, duration, endTime) {
     var table = document.getElementById("calendar");
-    var firstRow = true; //first row of each RN group has 1 extra cell for the pod group - need to track
-    for(var i=count*chairs+1; i < (count+1)*chairs+1; i++)
+    var maxCells = 9;
+    var offset = 8; //used to calculate row index from given hour
+    //RN number 'RNIndex' is responsible for 'chairs' number of chairs => this rn has 'chairs' + 1 total rows
+    //Each RN row group has a hidden row that is used to collapse/un-collapse the group
+    //Must not forget that table.rows[0] is the header
+    for(var i=RNIndex*chairs+2+RNIndex; i < (RNIndex+1)*chairs+2+RNIndex; i++)
     {
-        if(firstRow) var offset = 7; else var offset = 8;
         var row = table.rows[i];
 
         //block out times before nurse arrives
         var startTimeCell = row.cells[startTime.split(':')[0]-offset];
         var startTimePercent = (startTime.split(':')[1])/60;
-        if(firstRow) var j = 1; else var j = 0;
+        var j = 0;
         while(j < startTime.split(':')[0]-offset)
             AddFill(row.cells[j++],1,'unavailable',true);
         if(startTimePercent > 0)
@@ -37,14 +40,10 @@ function BuildRNRow(count, chairs, startTime, lunchTime, duration, endTime) {
             AddFill(row.cells[++endTimeIndex],1,'unavailable',true);
         else if(endTimePercent > 0)
             AddFill(row.cells[++endTimeIndex],1 - endTimePercent,'unavailable',false);
-        if(firstRow) var maxCells = 10; else var maxCells = 9;
         while (endTimeIndex++ < maxCells)
             AddFill(row.cells[endTimeIndex],1,'unavailable',true);
-
-        firstRow = false;
     }
 }
-
 function AddFill(cell, fraction, className, leftAligned){
     cell.style.padding = 0;
     var div = document.createElement('div');
@@ -55,5 +54,30 @@ function AddFill(cell, fraction, className, leftAligned){
     if(!leftAligned)
     {
         div.style.float = 'right';
+    }
+}
+
+function rowSelect(nurseIndex, chairs){
+    var rowGroup = document.getElementById("row-group-"+nurseIndex).cells[0];
+    var hiddenRow = $('#collapse-row-'+nurseIndex);
+    if(hiddenRow.is(':hidden'))
+    {
+        hiddenRow.show();
+        for(var i = 1; i <= chairs; i++)
+        {
+            var row = $('#RN-'+nurseIndex+'-row-'+i);
+            row.hide();
+        }
+        rowGroup.rowSpan = 1;
+    }
+    else
+    {
+        hiddenRow.hide();
+        rowGroup.rowSpan = chairs + 1;
+        for(var i = 1; i <= chairs; i++)
+        {
+            var row = $('#RN-'+nurseIndex+'-row-'+i);
+            row.show();
+        }
     }
 }
