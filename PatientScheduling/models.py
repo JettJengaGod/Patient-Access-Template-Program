dbc1b010673da2e8c791dc0c7535410fb026dfed
Rachel <rnchurch@ucdavis.edu>
2016-04-27 15:50:57 -0700

    Made drug time requirments optional

--- PatientScheduling/models.py
+++ PatientScheduling/models.py
@@ -57,5 +57,5 @@ class Appointment(models.Model):
 class ChemotherapyDrug(models.Model):
     # models.AutoField(primary_key=True) is a default field
     Name = models.CharField(unique=True, max_length=60, null=False)
-    EarliestTime = models.TimeField(auto_now=False, null=True)
-    LatestTime = models.TimeField(auto_now=False, null=True)
+    EarliestTime = models.TimeField(auto_now=False, null=True, blank=True)
+    LatestTime = models.TimeField(auto_now=False, null=True, blank=True)
